090ca005dd81e2457c8df00ef99132633c0cf114
rogerinn <rogeriosandrojunior@hotmail.com>
2021-04-03 02:55:34 -0300

    chore: config unit and integration test

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
