e308f931dd8e157bbeb5412172370f3b72bc15e6
rogerinn <rogeriosandrojunior@hotmail.com>
2021-04-04 21:58:38 -0300

    refactor: add paths

--- packages/api/src/presentation/protocols/controller.ts
+++ packages/api/src/presentation/protocols/controller.ts
@@ -1,4 +1,4 @@
-import { HttpResponse } from './http'
+import { HttpResponse } from '@api/presentation/protocols'
 
 export interface Controller<T = any> {
   handle: (request: T) => Promise<HttpResponse>
