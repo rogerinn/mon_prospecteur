346b3aec45806c366ca86aa40543d322c43b819e
rogerinn <rogeriosandrojunior@hotmail.com>
2021-04-06 15:48:48 -0300

    chore: switch api folder to framework

--- packages/framework/src/presentation/helpers/http-helper.ts
+++ packages/framework/src/presentation/helpers/http-helper.ts
@@ -1,4 +1,4 @@
-import { HttpResponse } from '@api/presentation/protocols'
+import { HttpResponse } from '@framework/presentation/protocols'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
