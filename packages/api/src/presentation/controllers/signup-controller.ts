154a475fb8a86468475e033ab92593974048f734
rogerinn <rogeriosandrojunior@hotmail.com>
2021-04-06 18:54:39 -0300

    feat: ensure Validation call with correct values

--- packages/api/src/presentation/controllers/signup-controller.ts
+++ packages/api/src/presentation/controllers/signup-controller.ts
@@ -4,6 +4,7 @@ export class SignUpController implements Controller {
   constructor (private readonly validation: Validation) { }
 
   async handle (request: SignUpController.Request): Promise<HttpResponse|null> {
+    this.validation.validate(request)
     return null
   }
 }
