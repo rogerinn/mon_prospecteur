2c34e31d300826d627b644703af0c1f2f433b193
rogerinn <rogeriosandrojunior@hotmail.com>
2021-04-07 00:59:58 -0300

    test: ensure SignUpController call error handling with correct error

--- packages/api/src/presentation/controllers/signup-controller.ts
+++ packages/api/src/presentation/controllers/signup-controller.ts
@@ -1,26 +1,28 @@
-import { badRequest, serverError } from '@/framework/src/presentation/helpers'
-import { Controller, HttpResponse, Validation } from '@/framework/src/presentation/protocols'
+import { IErrorHandling, serverError } from '@/framework/src/presentation/helpers'
+import { IController, HttpResponse, Validation } from '@/framework/src/presentation/protocols'
 
-export class SignUpController implements Controller {
-  constructor (private readonly validation: Validation) { }
-
-  async handle (request: SignUpController.Request): Promise<HttpResponse | null> {
-    try {
-      const error = this.validation.validate(request)
-      if (error) {
-        return badRequest(error)
-      }
-      return null
-    } catch (error) {
-      return serverError()
-    }
-  }
-}
-
-export namespace SignUpController {
+export namespace SignUp {
   export type Request = {
     email: string
     password: string
     confirmPassword: string
+  }
+
+  export class Controller implements IController {
+    constructor (private readonly validation: Validation,
+      private readonly errorHandler: IErrorHandling
+    ) { }
+
+    async handle (request: SignUp.Request): Promise<HttpResponse | null> {
+      try {
+        const error = this.validation.validate(request)
+        if (error) {
+          return this.errorHandler.handle(error)
+        }
+        return null
+      } catch (error) {
+        return serverError()
+      }
+    }
   }
 }
