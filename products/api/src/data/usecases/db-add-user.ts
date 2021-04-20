efd6be8e21876e978fb147014856c2a169e920ee
rogerinn <rogeriosandrojunior@hotmail.com>
2021-04-20 13:24:16 -0300

    feat: ensure DbAddUser call hasher with correct password

--- products/api/src/data/usecases/db-add-user.ts
+++ products/api/src/data/usecases/db-add-user.ts
@@ -1,10 +1,17 @@
 import { AddUser } from '@/products/api/src/domain/usecases/add-user'
 import { UserModel } from '@/products/api/src/domain/models/user'
 import { AddUserRepository } from '@/products/api/src/data/protocols/add-user-repository'
+import { Hasher } from '../protocols/cryptography/hasher'
 
 export class DbAddUser implements AddUser {
-  constructor (private readonly addUserRepository: AddUserRepository) {}
+  constructor
+  (
+    private readonly addUserRepository: AddUserRepository,
+    private readonly hasher: Hasher
+  ) { }
+
   async add (user: DbAddUser.Params): Promise<UserModel> {
+    await this.hasher.hash(user.password)
     return await this.addUserRepository.add(user)
   }
 }
