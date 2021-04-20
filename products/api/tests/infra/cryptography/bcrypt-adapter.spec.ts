b636fa230f398ecaefba79275ee02cbe9138fb3e
rogerinn <rogeriosandrojunior@hotmail.com>
2021-04-20 18:14:55 -0300

    test: ensure BcryptAdapter call hash with correct values

---
+++ products/api/tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,25 @@
+
+import { BcryptAdapter } from '@/products/api/src/infra/cryptography/bcrypt-adapter'
+import bcrypt from 'bcrypt'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return 'hash'
+  }
+}))
+
+const salt = 12
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
+describe('Bcrypt Adapter', () => {
+  describe('hash()', () => {
+    test('Should call hash with correct values', async () => {
+      const sut = makeSut()
+      const hashSpy = jest.spyOn(bcrypt, 'hash')
+      await sut.hash('any_value')
+      expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+    })
+  })
+})
